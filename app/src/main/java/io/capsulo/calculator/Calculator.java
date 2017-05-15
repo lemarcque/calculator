711b0ec820e975ac3866120ea0c8ff1a491cd406
lemarcque <hello@capsulo.io>
2017-05-15 14:22:23 +0200

    Ajout des fonctions trim & special Recuperation des operateurs et des valeurs des nombres (left|right) OK Replacement du nom d'application "calculator" dans la ressource string.xml

--- app/src/main/java/io/capsulo/calculator/Calculator.java
+++ app/src/main/java/io/capsulo/calculator/Calculator.java
@@ -4,6 +4,7 @@ import android.text.TextUtils;
 import android.util.Log;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 
 /**
@@ -50,14 +51,18 @@ public class Calculator {
         // Note: il faudrait plutôt vérifier si il existe un opérateur dans le calcul
         // findOperator() ....
         if(currentComputation.size() > 0) {
-            // Savoir si c'est une addition et/ou soustraction
-            // Si multi ou division
-            // priorite etc.
-            // efectuer l'additoin soustraction
-            // et voila : done.
-            updateCompute("");
+            // 1: Savoir si c'est une addition et/ou soustraction
+            // 2: Si multi ou division
+            // 3: priorite etc.
+            // 4: efectuer l'additoin soustraction
+            // 5: et voila : done.
+
+            updateCompute("");  // Update the value of the current writer calcul
+            trim();
 
             Log.i("calcul to calculate", currentComputation.toString());
+
+            // On attache les Tableau de chaïne de caractère ensemble
             String resultJoined = "-";
             String calculJoined = TextUtils.join(" ", currentComputation);
             results.put("result", resultJoined);
@@ -79,8 +84,87 @@ public class Calculator {
     }
 
     /* trouver un opérateur (signe) dans le calcul .. */
-    private void findOperator() {
+    private void trim() {
+        ArrayList<HashMap<String, String>> listOperators = new ArrayList<HashMap<String, String>>();
+        boolean toReplace = false;
+
+        for(int i = 0; i < currentComputation.size(); i++) {
+            if(currentComputation.get(i).toLowerCase().equals("x") || currentComputation.get(i).equals("÷")) {
+                toReplace = true;
+                listOperators.add(new HashMap<String, String>());
+                listOperators.get(listOperators.size() - 1).put("pos", String.valueOf(i));          // pos : position dans le tableau
+                listOperators.get(listOperators.size() - 1).put("value", currentComputation.get(i));// values : valeur du nombre ou autres
+            }
+        }
+
+        // Si la variable toReplace equals true, alors il faut "do stuff"
+        if(toReplace) {
+            replace(listOperators);
+        }else {
+            calculate();
+        }
+
     }
+
+    private void replace(ArrayList<HashMap<String, String>> operators) {
+        for (int i = 0; i < operators.size(); i++) {
+            boolean leftValuesNotNull = true;
+            boolean rightValuesNotNull = true;
+            int leftNumber;                     //  Le nombre se toruvant à la droite de l'opérateur
+            int rightNumber;                    // Le nombre se toruvant à la droite de l'opérateur
+            int index = 0;
+            ArrayList<String> currentLeftNumber = new ArrayList<String>();
+            String currentRightNumber = "";
+
+            while(leftValuesNotNull) {
+                index--;
+                int newpos = Integer.parseInt(operators.get(i).get("pos")) + index;     // Position de la valeur
+
+                if(newpos >= 0) {
+                    String values = currentComputation.get(newpos);
+                    if(!values.equals("x") && !values.equals("÷") && !values.equals("+") && !values.equals("-")) {
+                        currentLeftNumber.add(values);
+                    }else {
+                        leftValuesNotNull = false;
+                        index = 0;
+                    }
+                }else {
+                    leftValuesNotNull = false;
+                    index = 0;
+                }
+            }
+
+            while(rightValuesNotNull) {
+                index++;
+                int newpos = Integer.parseInt(operators.get(i).get("pos")) + index;     // Position de la valeur
+
+                if(newpos < currentComputation.size()) {
+                    String values = currentComputation.get(newpos);
+                    if(!values.equals("x") && !values.equals("÷") && !values.equals("+") && !values.equals("-")) {
+                        currentRightNumber += values;
+                    }else {
+                        rightValuesNotNull = false;
+                        index = 0;
+                    }
+                }else {
+                    rightValuesNotNull = false;
+                    index = 0;
+                }
+
+            }
+
+            Collections.reverse(currentLeftNumber);
+            Log.i("left", TextUtils.join("", currentLeftNumber));
+            Log.i("o", operators.get(i).get("value"));
+            Log.i("right", currentRightNumber);
+        }
+    }
+
+    private void calculate() {
+
+    }
+
+    // Rafraichit  / Rénitialise
     private void clear() {
         currentComputation.clear();
         currrentWritingNumber.clear();
